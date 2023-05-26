f8ac13bda4e6af0f82803171748008674041327c
dharmikpatel24 <pate4884@sherdiancollege.ca>
2023-05-25 20:10:10 -0400

    branch1

---
+++ src/helloworld2/Helloworld2.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package helloworld2;
+
+/**
+ *
+ * @author dharmik
+ */
+public class Helloworld2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+          // TODO code application logic here
+       int setscore = 20; 
+       System.out.print(setscore);
+    }
+    
+}
