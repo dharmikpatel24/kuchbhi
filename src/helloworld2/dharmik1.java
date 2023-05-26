5a2c4a7e8e3670ec06dcd527f7e96a2ee0c99bf0
dharmikpatel24 <pate4884@sherdiancollege.ca>
2023-05-25 20:29:49 -0400

    branch1

---
+++ src/helloworld2/dharmik1.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package helloworld2;
+
+/**
+ *
+ * @author dharmik
+ */
+public class dharmik1 {
+    public static void main(String[] args) {
+        // TODO code application logic here
+          // TODO code application logic here
+       int setscore = 30; 
+       System.out.print(setscore);
+        System.out.print("helloworld");
+        System.out.print("how are you");
+    }
+}
